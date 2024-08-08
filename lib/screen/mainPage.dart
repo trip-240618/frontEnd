322376ed8c61b83a715c6ba2eb5c93ad73325daa
heojiu <jiwoo12021@naver.com>
2024-08-08 23:54:24 +0900

    hoom 추가 08-08

--- lib/screen/mainPage.dart
+++ lib/screen/mainPage.dart
@@ -1,7 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SecondPage extends StatefulWidget {
