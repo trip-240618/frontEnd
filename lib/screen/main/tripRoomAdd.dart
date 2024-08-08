94dc662e3f449e8361b27552bdd991b53d76cf11
heojiu <jiwoo12021@naver.com>
2024-08-08 23:54:13 +0900

    hoom 추가 08-08

--- lib/screen/main/tripRoomAdd.dart
+++ lib/screen/main/tripRoomAdd.dart
@@ -1,11 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
 import 'package:tripStory/component/main/typeChoose.dart';
 import 'package:tripStory/util/color.dart';
-
 import '../../component/bottomContainer.dart';
 import '../../util/font.dart';
 
