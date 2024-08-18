ccffbf78cb7f7f9c535c215799d6e99cc9274f9b
Jiwoo <jiwoo12021@naver.com>
2024-08-19 00:02:59 +0900

    2024-08-19

--- lib/bottomNavigator.dart
+++ lib/bottomNavigator.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:get/get.dart';
 import 'package:tripStory/screen/tripHistory/tripHistoryMain.dart';
 import 'package:tripStory/screen/tripPlan/typeJ/jSchedulePage.dart';
 import 'package:tripStory/screen/tripPlan/typeP/pPlanPage.dart';
