91ec5de381a11e6b6f7b9d8e1864fa07df10cb91
Jiwoo <jiwoo12021@naver.com>
2024-10-30 23:54:46 +0900

    2024-10-30 알림 설정 기능 추가 & 여행방 삭제

--- lib/app/api/notificationApi.dart
+++ lib/app/api/notificationApi.dart
@@ -44,4 +44,47 @@ class ApiNotificationClient {
       rethrow;
     }
   }
+
+  ///알림 스위치 가져오기
+  Future<Map> getNotificationSetting() async {
+    try {
+      final response = await dioClient.dio.get(
+          '/notification/config'
+      );
+      if (response.statusCode == 200) {
+        final data = response.data;
+        print('data?? ${data}');
+        if(data.length==0){
+          return {};
+        }
+        return data;
+      } else {
+        throw Exception('Failed to auto-login: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error during auto-login: $e');
+      rethrow;
+    }
+  }
+  ///알림 스위치 변경하기
+  Future<Map> changeNotificationSetting(Map maps) async {
+    try {
+      final response = await dioClient.dio.put(
+          '/notification/config/modify',data: maps
+      );
+      if (response.statusCode == 200) {
+        final data = response.data;
+        print('data?? ${data}');
+        if(data.length==0){
+          return {};
+        }
+        return data;
+      } else {
+        throw Exception('Failed to auto-login: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error during auto-login: $e');
+      rethrow;
+    }
+  }
 }
