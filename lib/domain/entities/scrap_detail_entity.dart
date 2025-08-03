054395204f8b59746c10f14b835a48695ec5eb90
ShinDasom <zaqaziq@naver.com>
2025-08-04 00:47:23 +0900

    refactor: 스크랩 경로 수정

---
+++ lib/domain/entities/scrap_detail_entity.dart
@@ -0,0 +1,24 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:tripStory/data/models/response/scrap_detail_response.dart';
+
+part 'scrap_detail_entity.freezed.dart';
+part 'scrap_detail_entity.g.dart';
+
+@freezed
+abstract class ScrapDetailEntity with _$ScrapDetailEntity {
+  const factory ScrapDetailEntity({
+    required int id,
+    required String writerUuid,
+    required String nickname,
+    required String title,
+    required String content,
+    required bool hasImage,
+    required String color,
+    required bool bookmark,
+    required DateTime createDate,
+    required List<ScrapImages> imageDtos,
+  }) = _ScrapDetailEntity;
+
+  factory ScrapDetailEntity.fromJson(Map<String, dynamic> json) =>
+      _$ScrapDetailEntityFromJson(json);
+}
